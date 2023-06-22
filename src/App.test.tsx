1b48d1f459ebd443d3f444de5c692f8863e7475f
stanislav-revva <nivadrive1@gmail.com>
2023-06-22 14:28:01 +0300

    + test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { matrixSum, range } from './Util/number-functions';
+import LifeMatrix from './service/LifeMatrix';
+test("sum of matrix", () => {
+  expect(matrixSum([[1,2,3],
+                    [4,5,6]])).toEqual(21)
+})
+test("range test", () => {
+ expect(range(1,3)).toEqual([1,2])
+})
+const lifeMatrix = new LifeMatrix(expected);
+let expected = [[0,0,0,0],
+                [0,1,1,0],
+                [0,1,1,0],
+                [0,0,0,0],
+              ]
+test("range test", () => {
+  expect(expected ).toEqual(lifeMatrix.current.next())
+ })
